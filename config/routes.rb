55a6da1c96d8ad9daa03e83bf4e1efe67c5acde5
Hannes Holton <hannes@holtonfamily.net>
2014-06-09 16:29:36 +1000

    Add gravatar and FactoryGirl

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
